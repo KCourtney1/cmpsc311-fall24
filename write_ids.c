87859610eec6b0c5c72efcd870981e8c98143d24
kyle courtney <kylecourt2@gmail.com>
2024-09-09 19:11:43 -0400

    assigment 1 complete?

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "KCourtney1";
+    char STUDENT_ID[] = "971755630";
 
-    return 0 ;
+    return 0;
 }
